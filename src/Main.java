abb6035b9b71cd6a1b63ef3dd77337f3d0c42645
Maikoncarlos <maikonsaturno@gmail.com>
2023-05-30 20:54:15 -0300

    testando e integrando a classe de dominio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import com.gitHub.maikoncarlos.desafio.dominio.Content;
-import com.gitHub.maikoncarlos.desafio.dominio.Course;
-import com.gitHub.maikoncarlos.desafio.dominio.Mentoring;
+import com.gitHub.maikoncarlos.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -17,8 +15,8 @@ public class Main {
         courseKotlin.setDescription("Description Course - Kotlin POO ");
         courseKotlin.setDuration(7);
 
-        System.out.println(courseJava);
-        System.out.println(courseKotlin);
+//        System.out.println(courseJava);
+//        System.out.println(courseKotlin);
 
         Mentoring mentoringJava = new Mentoring();
         mentoringJava.setTitle("Title - Mentoring Java");
@@ -30,7 +28,44 @@ public class Main {
         mentoringKotlin.setDescription("Description Mentoring - Kotlin POO");
         mentoringKotlin.setNow_date(LocalDate.now().plusWeeks(2));
 
-        System.out.println(mentoringJava);
-        System.out.println(mentoringKotlin);
+//        System.out.println(mentoringJava);
+//        System.out.println(mentoringKotlin);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Bootcamp Java Developer");
+        bootcamp.setDescription("Description Bootcamp Java Developer");
+        bootcamp.getContentes().add(courseJava);
+        bootcamp.getContentes().add(courseKotlin);
+
+        Dev devCamila = new Dev();
+        devCamila.setName(" Camila ");
+        devCamila.subscribeBootcamp(bootcamp);
+        System.out.println(" Conteudos inscritos Camila: " + devCamila.getSubscribedContent());
+        devCamila.toProgress();
+        System.out.println(" Camila finalizou: " + devCamila.getFinishedContent());
+
+        System.out.println(" ");
+
+        System.out.println(" Conteudos inscritos Camila: " + devCamila.getSubscribedContent());
+        devCamila.toProgress();
+        System.out.println(" Camila finalizou: " + devCamila.getFinishedContent());
+        System.out.println(" ");
+        System.out.println(" Total de XP da Camila: " + devCamila.calculateTotalXP());
+
+        System.out.println(" ");
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println(" ");
+
+
+        Dev devJoao= new Dev();
+        devJoao.setName(" Joao ");
+        devJoao.subscribeBootcamp(bootcamp);
+        System.out.println(" Conteudos inscritos Joao: " + devJoao.getSubscribedContent());
+        devJoao.toProgress();
+        System.out.println(" Joao finalizou: " + devJoao.getFinishedContent());
+
+        System.out.println(" ");
+        System.out.println(" Total de XP do Joao: " + devJoao.calculateTotalXP());
+
     }
 }
